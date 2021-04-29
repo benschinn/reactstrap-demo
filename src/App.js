b5aff53e7ba30ca5a0c6da1780ba663ba0dd4395
Ben Schinn <ben.schinn@mx.com>
2021-04-28 21:50:49 -0400

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
-function App() {
+import { Card, CardImg, CardText, CardBody, CardTitle, Container, Row, Col } from 'reactstrap'
+
+const App = () => {
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+    getPokemons()
+    async function getPokemons() {
+      try {
+        const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=200')
+
+        console.log(data)
+
+        const pokemonArr = data.results.map(async (pokemon) => {
+          const { name, url } = pokemon;
+          const { data } = await axios.get(url)
+          const { front_default } = data.sprites
+
+          return {
+            name,
+            image: front_default,
+            weight: data.weight
+          }
+        })
+
+        Promise.all(pokemonArr)
+        .then(data => setPokemons(data))
+      } catch(error) {
+        console.log(error)
+      }
+    }
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <h1>Pokedex</h1>
+      <Row>
+        {pokemons.map(pokemon => (
+          <PokemonCard key={pokemon.name} pokemon={pokemon} />))
+        }
+      </Row>
+    </Container>
+  )
+}
+
+const PokemonCard = (props) => {
+  const { name, image, weight } = props.pokemon
+  return (
+    <Col xs="12" sm="6" md="4" lg="3">
+      <Card>
+        <CardBody>
+          <CardImg src={image}/>
+          <CardTitle tag="h3">{name}</CardTitle>
+          <CardText>weight: {weight}kg</CardText>
+        </CardBody>
+      </Card>
+    </Col>
+  )
 }
 
 export default App;
